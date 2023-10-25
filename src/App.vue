44507e3f503e86a7e8ed7a75e0f13fe00f10ae02
bboysquiz <mrkiraller@gmail.com>
2023-10-25 16:37:04 +0300

    created tabswitcher

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-
+import TabSwitcher from './components/TabSwitcher.vue';
+import Post from './components/Post.vue';
+import User from './components/User.vue';
 </script>
 
 <template>
-  
+  <TabSwitcher />
+  <Post />
+  <User />
 </template>
 
 <style scoped>
--- src/components/TabSwitcher.vue
+++ src/components/TabSwitcher.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import { useStore } from 'vuex';
 
+const store = useStore();
+
+const setTab = (tab: string) => {
+    store.state.currentTabComponent = tab;
+};
 </script>
 
 <template>
-  
+    <div>
+        <button @click="setTab('Posts')">Posts</button>
+        <button @click="setTab('Users')">Users</button>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
